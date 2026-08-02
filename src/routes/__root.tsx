4ab47a824cdf734cba1694522f613bcec531b928
checktheprice <support.editexa@gmail.com>
2026-08-02 20:22:05 +0530

    Integrate Google Analytics tracking
    Added Google Analytics tracking script to the root component.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -107,6 +107,22 @@ function RootShell({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+        {/* Google Analytics */}
+<script
+  async
+  src="https://www.googletagmanager.com/gtag/js?id=G-BJ6VXQGZ37"
+></script>
+
+<script
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-BJ6VXQGZ37');
+    `,
+  }}
+/>
       </head>
       <body>
         {children}
